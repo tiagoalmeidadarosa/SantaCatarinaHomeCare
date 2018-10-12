3aad8cf697edbfef4f5b28efc487ba33e77ab3e7
Tiago Almeida da Rosa <tiago_almeida_rosa@hotmail.com>
2018-10-12 17:34:29 -0300

    Textos das dúvidas referentes ao site e coordenadas do google maps

--- SantaCatarinaHomeCare/js/custom.js
+++ SantaCatarinaHomeCare/js/custom.js
@@ -6,8 +6,8 @@ jQuery(document).ready(function () {
     //this code is for the gmap
     var map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333
+        lat: -29.9144333,//-12.043333,
+        lng: -51.0427995//-77.028333
     });
 
 
